c80c9c919dd04b633508249535aaa4693ac75816
Selvam <skrgds@gmail.com>
2024-02-04 19:55:01 +0530

    COmmit to check poll SCM

--- src/main/java/com/leaftaps/testcases/DemoTC.java
+++ src/main/java/com/leaftaps/testcases/DemoTC.java
@@ -5,8 +5,8 @@ public class DemoTC {
 	public static void main(String[] args) {
 
 		System.out.println("Test line inclusion");
-		int a = 10;
-		int b = 30;
+		int a = 50;
+		int b = 40;
 		int c = a + b;
 		System.out.println("The sum is " +c);
 		System.out.println("This is an addition operation");
