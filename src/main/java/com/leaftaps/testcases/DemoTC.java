dd841ae64324cec683ae1c4affe231a8810d4a90
Selvam-Yuvi <skrgds@gmail.com>
2024-02-04 16:02:55 +0530

    Update DemoTC.java

--- src/main/java/com/leaftaps/testcases/DemoTC.java
+++ src/main/java/com/leaftaps/testcases/DemoTC.java
@@ -5,7 +5,7 @@ public class DemoTC {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Test line inclusion");
+		System.out.println("Test line inclusion editing from repo");
 
 	}
 
