2050b2309bfc9e6909eb65f4fff74fa573cc833e
Selvam <skrgds@gmail.com>
2024-02-04 19:43:49 +0530

    Latest commit

--- src/main/java/com/leaftaps/testcases/DemoTC.java
+++ src/main/java/com/leaftaps/testcases/DemoTC.java
@@ -5,8 +5,8 @@ public class DemoTC {
 	public static void main(String[] args) {
 
 		System.out.println("Test line inclusion");
-		int a = 5;
-		int b = 10;
+		int a = 10;
+		int b = 30;
 		int c = a + b;
 		System.out.println("The sum is " +c);
 		System.out.println("This is an addition operation");
