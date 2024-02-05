62757a4b36d4a86ca1dd1e5516063884c00c8c3f
Selvam <skrgds@gmail.com>
2024-02-05 19:36:56 +0530

    Added subtraction

--- src/main/java/com/leaftaps/testcases/DemoTC.java
+++ src/main/java/com/leaftaps/testcases/DemoTC.java
@@ -9,6 +9,7 @@ public class DemoTC {
 		int b = 40;
 		int c = a + b;
 		int d = a * b;
+		int x = a - b;
 		System.out.println("The multiplication is " +d);
 		System.out.println("The sum is " +c);
 		System.out.println("This is an addition operation");
